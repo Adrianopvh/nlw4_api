36d0fa9ec11c86d3785c91c90d7a8fbb28d13906
Adrianopvh <adriano.rodriques12389@gmail.com>
2021-02-28 16:13:51 -0400

    Finalizando Envio de e-mail

--- src/routes.ts
+++ src/routes.ts
@@ -7,14 +7,11 @@ const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
-
 const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
-
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
-
 router.post("/sendMail", sendMailController.execute);
 
 export { router };
